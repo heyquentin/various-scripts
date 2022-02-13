60cec9e84692f222e061b65886d7e96a896b5b9b
heyquentin <quentingauthier@gmail.com>
2022-02-13 10:07:59 -0800

    Reads in contents from file & prints to terminal
    There's no wrapping on this one

---
+++ animate.py
@@ -0,0 +1,24 @@
+#Text Animation -  www.101computing.net/text-based-animations/
+import os
+import time
+
+def getcontents():
+    with open('contents.txt', 'r') as file:
+        data = file.read().strip()
+        return data
+
+def animate_text(text):
+  number_of_characters=1
+  while True:
+    print("\n")
+    print(text[0:number_of_characters])
+    number_of_characters += 1
+    if number_of_characters > len(text):
+      number_of_characters = 0
+      text = str(getcontents())
+      time.sleep(10)
+    time.sleep(0.1)
+    os.system('clear')
+
+#Main Program Starts Here....
+animate_text(str(getcontents()))
